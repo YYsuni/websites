7d5884d4a230ef49b6b958a43b6cb8a26baf8219
Suni <YYsuni1001@gmail.com>
2023-09-13 18:22:23 +0800

    Root page

---
+++ prettier.config.cjs
@@ -0,0 +1,13 @@
+/* eslint-disable no-undef */
+module.exports = {
+	jsxSingleQuote: true,
+	jsxBracketSameLine: true,
+	printWidth: 120,
+	singleQuote: true,
+	trailingComma: 'none',
+	useTabs: true,
+	tabWidth: 2,
+	semi: false,
+	arrowParens: 'avoid',
+	plugins: ['prettier-plugin-tailwindcss']
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,24 @@
 import type { Config } from 'tailwindcss'
+import plugin from 'tailwindcss/plugin'
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}'
+	],
+	theme: {
+		extend: {}
+	},
+	plugins: [
+		plugin(function ({ addComponents }) {
+			addComponents({
+				'.border-text': {
+					color: 'transparent',
+					'-webkit-text-stroke': '2px #2b2b2b'
+				}
+			})
+		})
+	]
 }
 export default config
