9b3a41a63a11d645d20cf6c4138b3a1a59ad2a3c
Suni <YYsuni1001@gmail.com>
2023-09-13 18:31:24 +0800

    New page: Nextjs CPU Animation

---
+++ src/app/next.js-cpu-animation/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+	return (
+		<main className='flex min-h-screen flex-col items-center justify-center'>
+			<a target='_blank' href='https://nextjs.org/' className='font-medium text-sky-500 hover:underline'>
+				Link
+			</a>
+			CPU
+		</main>
+	)
+}
