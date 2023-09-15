c4e315329d7d66162a74c8954e8272530602849d
Suni <YYsuni1001@gmail.com>
2023-09-15 17:47:01 +0800

    Website 1: Planit, stars and rotation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,17 @@ export default function Home() {
 	return (
 		<main className='flex h-screen flex-col items-center justify-center'>
 			<h1 className='border-text font-mono text-5xl font-bold'>WEBSITES</h1>
-			<ol className='mt-4 gap-2'>
+			<ol className='mt-4 flex flex-wrap gap-4'>
 				<li>
-					<Link href='/next.js-cpu-animation' className='border-2 border-gray-600 p-2 hover:underline'>
-						CPU
+					<Link href='/next.js-cpu-animation' className='inline-block border-2 border-gray-600 p-2 hover:underline'>
+						1. CPU
+					</Link>
+				</li>
+				<li>
+					<Link
+						href='/framer-motion-usescroll-3d'
+						className='inline-block border-2 border-gray-600 p-2 hover:underline'>
+						2. 3D and Scroll
 					</Link>
 				</li>
 			</ol>
